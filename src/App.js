188e8e25f7a1e6bd9ea5084f382df31ef3a58656
MadeAirawata <airawatamade@gmail.com>
2024-05-26 01:52:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Forgotpassword from './components/Forgotpassword';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Forgotpassword">
+        <Forgotpassword />
+      </div>
+    </div>
+  );
+}
+
+export default App;
